22556d23f7197428677e7dd9e84d9ad7fd1cfcbd
Manan patel <immnan333@gmail.com>
2026-04-08 20:29:12 +0530

    hawk setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/immnan/hawk
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/immnan/hawk/pkg"
+)
+
+func main() {
+
+	fmt.Println("Initializing the hawk...")
+	pkg.Run()
+
+}
